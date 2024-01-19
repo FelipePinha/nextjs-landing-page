dabc8648a6211f5b5c51a3970d6859ae6ba97225
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-01-19 15:23:05 -0300

    add full button theme

--- src/app/components/button/Button.tsx
+++ src/app/components/button/Button.tsx
@@ -2,11 +2,16 @@ import styles from './Button.module.scss';
 
 interface ButtonProps {
     title: string;
-    theme: 'primary' | 'secondary';
+    theme: 'primary' | 'secondary' | 'full';
 }
 
 export const Button = ({ title, theme }: ButtonProps) => {
-    const generateClassByTheme = () => (theme === 'primary' ? styles.primary : styles.secondary);
+    const generateClassByTheme = () => {
+        if (theme === 'secondary') return styles.secondary;
+        if (theme === 'full') return styles.full;
+
+        return styles.primary;
+    };
 
     return <button className={`${styles.button} ${generateClassByTheme()}`}>{title}</button>;
 };
