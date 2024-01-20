7b9d54feeff543cf1d459eaea278c191a13d8e21
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-01-20 15:45:01 -0300

    responsive adjustments on welcome component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <>
             <Header />
-            {/* <Welcome /> */}
+            <Welcome />
             {/* <Features /> */}
             {/* <Contact /> */}
             {/* <Footer /> */}
