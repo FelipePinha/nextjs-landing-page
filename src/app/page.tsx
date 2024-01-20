8c4993708a9a1e9585a90c2ddfbd915ec3dc46d8
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-01-20 16:58:45 -0300

    responsive adjustments on footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <Welcome />
             <Features />
             <Contact />
-            {/* <Footer /> */}
+            <Footer />
         </>
     );
 }
