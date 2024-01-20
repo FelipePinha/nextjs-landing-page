b016f7871b124ae22dcbc79f6d5f6bad27196289
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-01-20 16:23:20 -0300

    responsive adjustments on features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <>
             <Header />
             <Welcome />
-            {/* <Features /> */}
+            <Features />
             {/* <Contact /> */}
             {/* <Footer /> */}
         </>
